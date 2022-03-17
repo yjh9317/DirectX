5ab22ca9f867be95cc4b6519759a5d89769d4a9a
yjh9317 <66587512+yjh9317@users.noreply.github.com>
2022-03-18 00:45:48 +0900

    CDevice

---
+++ CDevice.h
@@ -0,0 +1,35 @@
+#pragma once
+
+class CDevice
+{
+	SINGLE(CDevice);
+private:
+	HWND							m_hWnd; // Main Window Handle
+	Vec2							m_vRenderResolution; // 렌더링 해상도
+
+	//ID3D11Device*	m_pDevice;
+	ComPtr<ID3D11Device>			m_pDevice;			// GPU 메모리 제어,생성하고 관리
+	ComPtr<ID3D11DeviceContext>		m_pDeviceContext;	// GPU Rendering 제어 
+
+	ComPtr<IDXGISwapChain>			m_pSwapChain;
+	ComPtr<ID3D11Texture2D>			m_pRenderTarget;
+	ComPtr<ID3D11Texture2D>			m_pDepthStencilTarget;
+
+	ComPtr<ID3D11RenderTargetView>	m_RTV;
+	ComPtr<ID3D11DepthStencilView>	m_DSV;
+
+	D3D11_VIEWPORT					m_tViewPort;
+	DXGI_SWAP_CHAIN_DESC			m_tSwapChainDesc;
+
+public:
+	int init(HWND _hWnd, Vec2 _vRenderResolution);
+
+	void Present() { m_pSwapChain->Present(0, 0); }		// 백버퍼와 프론트 버퍼 바꾸는 함수
+	void ClearTarget();									// 타겟(버퍼)를 클리어
+
+private:
+	int CreateSwapchain();
+	int CreateView();
+
+};
+
